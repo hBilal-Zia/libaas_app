51cf7550a00cccbf08e07a6bba589cb9c535d836
shoaib zaman <shoaibeddie@gmail.com>
2024-04-27 15:07:17 +0500

    changes updated

--- lib/views/splash/splash_controller.dart
+++ lib/views/splash/splash_controller.dart
@@ -9,6 +9,7 @@ import 'package:libaas_app/views/auth/verify_email/verify_email.dart';
 import 'package:libaas_app/views/home_screen/nav_bar.dart';
 import 'package:libaas_app/views/onboading_screen/onboarding.dart';
 import 'package:libaas_app/views/profile/profile_screen.dart';
+import 'package:libaas_app/views/splash/splash.dart';
 
 class SplashController extends GetxController {
   final auth = FirebaseAuth.instance;
@@ -20,7 +21,7 @@ class SplashController extends GetxController {
       if (auth.currentUser != null) {
         Get.offAll(const NavBar());
       } else {
-        Get.to(ProfileScreen());
+        Get.to(const OnboardingScreen());
       }
     });
   }
